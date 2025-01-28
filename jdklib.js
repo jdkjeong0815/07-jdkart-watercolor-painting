23e47de192f8a2e1ee124f5333625d2196a881f4
dkjeong0815 <dkjeong0815@gmail.com>
2025-01-29 00:50:03 +0900

    jpg to png 수정

--- jdklib.js
+++ jdklib.js
@@ -2,7 +2,7 @@
 function keyPressed() {
     if (key === 's' || key === 'S') {
       let timestamp = nf(year(), 2) + nf(month(), 2) + nf(day(), 2) + "-" + nf(hour(), 2) + nf(minute(), 2) + nf(second(), 2);
-      saveCanvas(saveFileName + "-" + timestamp, "jpg");
+      saveCanvas(saveFileName + "-" + timestamp, "png");
     }
   }
   
